c07fd287828f31a781d2936649b777e1b3328d89
Tomas Sesztak <tsesztak@gmail.com>
2020-05-25 17:38:48 +0200

    Added support for fe command to be launched at current directory

--- scripts_edit.sh
+++ scripts_edit.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-file="$(find ${1} -type f |grep -v -e '.git' |fzf -i)"
+findPath="${1:-$(pwd)}"
+file="$(find ${findPath} -type f 2>/dev/null |grep -v -e '.git' |fzf -i)"
 [ ! -z $file ] && vim $file
