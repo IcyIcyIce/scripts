f0ffe3be27119e7e2baa564e331475e86716b8f7
Tomas Sesztak <tsesztak@gmail.com>
2020-05-25 17:48:43 +0200

    scripts_edit.sh now good with shellcheck

--- scripts_edit.sh
+++ scripts_edit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 findPath="${1:-$(pwd)}"
-file="$(find ${findPath} -type f 2>/dev/null |grep -v -e '.git' |fzf -i)"
-[ ! -z $file ] && vim $file
+file="$(find "${findPath}" -type f 2>/dev/null |grep -v -e '.git' |fzf -i)"
+[ -n "$file" ] && vim "$file"
