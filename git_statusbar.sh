cd420aebb87aacb5da570af09cde0b659d0cf3d3
Tomas Sesztak <tsesztak@gmail.com>
2020-06-05 15:26:04 +0200

    Reworked git_statusbar.sh

--- git_statusbar.sh
+++ git_statusbar.sh
@@ -10,8 +10,18 @@ result_color="$FG_GREEN"
 git_branch="$(git branch |cut -d' ' -f2)"
 
 # Number of modified files
-git_modified="$(git status |grep 'modified:' |wc -l)"
-[ "$git_modified" -gt 0 ] && result_color="$FG_RED"
+#git_modified="$(git status |grep 'modified:' |wc -l)"
+#[ "$git_modified" -gt 0 ] && result_color="$FG_RED"
+
+# Change color according to status
+## Untracked files
+git_status="$(git status)"
+git_committed="$(echo $git_status |grep -c 'Changes to be committed')"
+[ "$git_committed" -ne 0 ] && result_color="$FG_YELLOW"
+git_untracked="$(echo $git_status |grep -c Untracked)"
+[ "$git_untracked" -ne 0 ] && result_color="$FG_RED" && append_text+="*"
+git_modified="$(echo $git_status |grep -c 'Changes not staged')"
+[ "$git_modified" -ne 0 ] && result_color="$FG_RED"
 
 result="${BOLD}${result_color}${git_branch}"
-echo -e $result
+echo -e "${result}${append_text}"
