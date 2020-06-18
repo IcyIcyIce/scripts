a2969e701ea1a1c1088845b5ea6be481f66ae7b0
Tomas Sesztak <tsesztak@gmail.com>
2020-06-18 11:37:42 +0200

    Fixed formatting issue in bash profile

--- bash/profile.sh
+++ bash/profile.sh
@@ -1,3 +1,3 @@
 source "${SCRIPTS_DIR}/bash/colors.sh"
 
-export PS1="${FG_BLUE}${BOLD} [ ${FG_LIGHTBLUE}\u${FG_YELLOW}@${FG_MAGENTA}\h ${FG_BLUE}] ${FG_BLUE}\w \$(git_status)\n ${FG_LIGHTRED}\$${RESET} "
+export PS1="${FG_BLUE}${BOLD} [ ${FG_LIGHTBLUE}\u${FG_YELLOW}@${FG_MAGENTA}\h ${FG_BLUE}] ${FG_BLUE}\w ${RESET}\$(git_status)\n ${FG_LIGHTRED}\$${RESET} "
