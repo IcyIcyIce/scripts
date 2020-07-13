6e8a326193798005f4bfb9f561c85f63154e3e8f
Tomas Sesztak <tsesztak@gmail.com>
2020-07-13 14:04:56 +0200

    Fixed broken git_status() colors in PS1

--- bash/profile.sh
+++ bash/profile.sh
@@ -1,3 +1,3 @@
 source "${SCRIPTS_DIR}/bash/colors.sh"
 
-export PS1="${FG_BLUE}${BOLD} [ ${FG_LIGHTBLUE}\u${FG_YELLOW}@${FG_MAGENTA}\h ${FG_BLUE}] ${FG_BLUE}\w ${RESET}\$(git_status)\n ${FG_LIGHTRED}\$${RESET} "
+export PS1="${PS1_FG_BLUE}${PS1_BOLD} [ ${PS1_FG_LIGHTBLUE}\u${PS1_FG_YELLOW}@${PS1_FG_MAGENTA}\h ${PS1_FG_BLUE}] ${PS1_FG_BLUE}\w ${PS1_RESET}\$(git_status)\n ${PS1_FG_LIGHTRED}\$${PS1_RESET} "
