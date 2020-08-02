9f3be03a52f0524566552b676338331f5172234e
Tomas Sesztak <tsesztak@gmail.com>
2020-08-02 13:16:32 +0200

    fixed colors for tmux and added custom .tmux.conf

--- modules/tmux.sh
+++ modules/tmux.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+alias tmux="tmux -f ${SCRIPTS_DIR}/dotfiles/.tmux.conf"
+
 if [ -z "$TMUX" ]; then
   tmux has-session -t ${USER}
   [ $? -eq 0 ] && tmux attach-session -t "${USER}" || tmux new-session -s "${USER}"
