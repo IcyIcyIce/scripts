91c58c81c9231bd13f164cf05ca3aebb7c1e2850
Tomas Sesztak <tsesztak@gmail.com>
2020-11-18 14:19:11 +0100

    Renamed variable for disables

--- modules.sh
+++ modules.sh
@@ -1,5 +1,5 @@
 source "${__SCRIPTS_DIR}/modules/brew.sh"
 source "${__SCRIPTS_DIR}/modules/git_statusbar.sh"
-[ -z "$__FZF_DISABLED" ] && source "${__SCRIPTS_DIR}/modules/scripts_edit.sh"
+[ -z "$__DISABLE_FZF" ] && source "${__SCRIPTS_DIR}/modules/scripts_edit.sh"
 source "${__SCRIPTS_DIR}/modules/weather.sh"
 source "${__SCRIPTS_DIR}/modules/tmux/tmux.sh"
